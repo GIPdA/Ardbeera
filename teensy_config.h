9e67cd5eead3355b002c391997e833d4f8dd1ee3
BBenj <balga.benjamin@gmail.com>
2013-02-04 20:05:40 +0100

    Teensy 3.0 board definitions

---
+++ teensy_config.h
@@ -0,0 +1,18 @@
+/**
+* @file teensy_config.h
+* @brief Fichier de définition des paramètres de configuration de la Teensy 3.0
+*
+* @date 4 février 2013
+* @author BBenj
+*
+*/
+
+#ifndef TEENSY_CONFIG_H
+#define TEENSY_CONFIG_H
+
+
+///
+#define ONBOARD_LED	13	///< Teensy onboard led pin
+
+
+#endif // TEENSY_CONFIG_H
